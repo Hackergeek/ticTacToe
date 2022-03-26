f43b3e3863c98fe89d6b0b8cfaf7f00e4ac9e4ca
skyward <skyward@txzing.com>
2022-03-26 15:06:52 +0800

    migrate to androidx and upgrade gradle version
    (cherry picked from commit 7ed1d5b21db58f5c7434c237a5d7289fe5223ff8)

--- app/src/main/java/com/acme/tictactoe/controller/TicTacToeActivity.java
+++ app/src/main/java/com/acme/tictactoe/controller/TicTacToeActivity.java
@@ -1,6 +1,6 @@
 package com.acme.tictactoe.controller;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
